3e557061de25cf7e6dcda93baf086bad52402066
Xu Yiming <xuyiming.open@outlook.com>
2024-04-14 16:31:02 +0800

    feat: Add files

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import Layout from "./components/layout";
+import Header from "./components/header";
+import Main from "./components/main";
+
+export default function App() {
+	return <Layout>
+		<Header />
+		<Main />
+	</Layout>
+}
