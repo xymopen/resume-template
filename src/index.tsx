715cd4c683c79faa935bfae67d11f08f771517bc
Xu Yiming <xuyiming.open@outlook.com>
2024-04-14 16:31:02 +0800

    feat: Add files

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import { render } from 'preact';
 import { MDXProvider } from '@mdx-js/preact'
-import App from "./app.mdx"
-import Layout from "./components/layout"
-
-const components = {
-	wrapper: Layout
-}
+import App from "./app"
+import "./index.css"
 
 render(
-	<MDXProvider components={components}>
+	<MDXProvider>
 		<App />
 	</MDXProvider>,
 	document.getElementById("app")!
