72d97ea625cbcd2132ef001e765a17af638cd078
Xu Yiming <xuyiming.open@outlook.com>
2024-04-18 01:48:31 +0800

    refactor: Use <img> instead of background-image in <ImageIcon>

--- src/components/image-icon/index.tsx
+++ src/components/image-icon/index.tsx
@@ -3,5 +3,5 @@ import "./index.css"
 type ImageIconProps = { src?: string }
 
 export default function ImageIcon({ src }: ImageIconProps) {
-	return <span class="icon" style={{ backgroundImage: `url(${src})` }} />;
+	return <span class="icon"><img src={src} /></span>;
 }
